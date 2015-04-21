f452d42bc10cf8de44657ca021a72d1ea70fdc66
kazikai <junghyun.han@sk.com>
2015-04-21 21:58:34 +0900

    kakaotok 제목수정

--- main.js
+++ main.js
@@ -93,19 +93,18 @@ $( document ).on( "pagechange",  function(){
   /* kakao톡 링크 */
   // 사용할 앱의 Javascript 키를 설정해 주세요.
   Kakao.init('abeaf346736c57a8ba07ef2dcfcad028');
-
   // 카카오톡 링크 버튼을 생성합니다. 처음 한번만 호출하면 됩니다.
   Kakao.Link.createTalkLinkButton({
     container: '#kakao-link',
-    label: '카카오링크 샘플에 오신 것을 환영합니다.',
+    label: '저희의 결혼을 축하해주세요',
     image: {
       src: 'http://dn.api1.kage.kakao.co.kr/14/dn/btqaWmFftyx/tBbQPH764Maw2R6IBhXd6K/o.jpg',
       width: '300',
       height: '200'
     },
     webButton: {
-      text: '모바일 청첩장 바로가기',
-      url: 'http://dev.kazikai.net/invitation' // 앱 설정의 웹 플랫폼에 등록한 도메인의 URL이어야 합니다.
+      text: '청첩장 바로가기',
+      url: 'http://dev.kazikai.net/invitation/index.html' // 앱 설정의 웹 플랫폼에 등록한 도메인의 URL이어야 합니다.
     }
   });
 });
